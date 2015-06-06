9c22b793a3f04f6800994ec90cea957afc5ff60d
nikitakuz <skateaddict@gmail.com>
2015-06-06 11:02:42 -0700

    Add app health check handlers.

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ app.get('/', function(req, res) {
   res.status(200).send('Hello, google cloud!');
 });
 
+/* Include the app engine handlers to respond to start, stop, and health checks. */
+app.use(require('./lib/appengine-handlers'));
+
 var server = app.listen(process.env.PORT || '5000', '0.0.0.0', function() {
   var a = server.address();
   console.log('App listening at http://%s:%s', a.address, a.port);
