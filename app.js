57265095ce1ca7a72e742a60f000d8de16b2d767
nikitakuz <skateaddict@gmail.com>
2015-06-05 19:07:45 -0700

    Simple NodeJS app (Hello, google cloud\!).

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res) {
+  res.status(200).send('Hello, google cloud!');
+});
+
+var server = app.listen(process.env.PORT || '5000', '0.0.0.0', function() {
+  var a = server.address();
+  console.log('App listening at http://%s:%s', a.address, a.port);
+  console.log("Press Ctrl+C to quit.");
+});
