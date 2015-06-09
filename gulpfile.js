45fc2c543643ae34ebdbfbf03a72d687d65ccbdf
nikitakuz <skateaddict@gmail.com>
2015-06-08 21:10:41 -0700

    Basic style and build.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 var gulp = require('gulp');
 var jade = require('gulp-jade');
+var less = require('gulp-less');
 
 var SRC = './client/src/';
 var BUILD = './client/dist/';
 
 var paths = {
-  jade: SRC + '**/*.jade'
+  jade: SRC + '**/*.jade',
+  less: SRC + '**/*.less'
 };
 
 gulp.task('jade', function() {
@@ -14,10 +16,17 @@ gulp.task('jade', function() {
     .pipe(gulp.dest(BUILD));
 });
 
-gulp.task('build', ['jade']);
+gulp.task('less', function() {
+  return gulp.src(paths.less)
+    .pipe(less())
+    .pipe(gulp.dest(BUILD));
+});
+
+gulp.task('build', ['jade', 'less']);
 
 gulp.task('watch', ['build'], function() {
-  gulp.watch(paths.jade, ['jade'])
+  gulp.watch(paths.jade, ['jade']);
+  gulp.watch(paths.less, ['less']);
 });
 
 gulp.task('default', ['watch']);
