660a48b3f8271bb291b8484cc9709312954c54ba
nikitakuz <skateaddict@gmail.com>
2015-06-08 18:46:12 -0700

    Switch hosting from google cloud to heroku.

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ app.get('/', function(req, res) {
   res.status(200).send('Hello, heroku!');
 });
 
-/* Include the app engine handlers to respond to start, stop, and health checks. */
-app.use(require('./lib/appengine-handlers'));
-
 var server = app.listen(process.env.PORT || '5000', '0.0.0.0', function() {
   var a = server.address();
   console.log('App listening at http://%s:%s', a.address, a.port);
