919b254888d4d95bad03e03abb6ad21b2a9d0e12
nikitakuz <skateaddict@gmail.com>
2015-06-08 21:54:01 -0700

    build: add lint and scripts tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var jade = require('gulp-jade');
 var less = require('gulp-less');
+var jshint = require('gulp-jshint');
+
 
 var SRC = './client/src/';
 var VENDOR = SRC + 'vendor/';
@@ -8,7 +10,8 @@ var DIST = './client/dist/';
 
 var paths = {
   jade: [SRC + '**/*.jade', '!' + VENDOR],
-  less: [SRC + '**/*.less', '!' + VENDOR]
+  less: [SRC + '**/*.less', '!' + VENDOR],
+  scripts: [SRC + '**/*.js', '!' + VENDOR]
 };
 
 gulp.task('jade', function() {
@@ -21,6 +24,17 @@ gulp.task('less', function() {
   return gulp.src(paths.less)
     .pipe(less())
     .pipe(gulp.dest(DIST));
+});
+
+gulp.task('lint', function() {
+  return gulp.src(paths.scripts)
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('scripts', ['lint'], function() {
+  return gulp.src(paths.scripts)
+    .pipe(gulp.dest(DIST))
 });
 
 gulp.task('build', ['jade', 'less']);
