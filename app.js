aa5a8a7f6872e8cca930158d442681806a8a66dd
nikitakuz <skateaddict@gmail.com>
2015-06-08 18:44:37 -0700

    Switch hosting from google cloud to heroku.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-  res.status(200).send('Hello, google cloud!');
+  res.status(200).send('Hello, heroku!');
 });
 
 /* Include the app engine handlers to respond to start, stop, and health checks. */
