8c5b359051669da9edce37df5cfdbee980f6a9b4
Nikita Kuznetsov <skateaddict@gmail.com>
2018-08-01 18:38:02 -0700

    remove unutilized and website breaking firebase dependency

--- server/routes/homepage/index.js
+++ server/routes/homepage/index.js
@@ -1,16 +1,12 @@
 var path = require('path');
 var jade = require('jade');
-var data = rootRequire('server/lib/data');
 
 module.exports = {
   render: function(request, response) {
-    data.getHomepage(function(data) {
-      var indexPath = path.join(process.cwd(), '/client/src/index.jade');
-      var fn = jade.compileFile(indexPath, {});
-      var html = fn({ homepage: data });
-      response.writeHead(200, { 'Content-Type': 'text/html' });
-      response.write(html, 'binary');
-      response.end();
-    });
+    var indexPath = path.join(process.cwd(), '/client/src/index.jade');
+    var fn = jade.compileFile(indexPath, {});
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write(fn(), 'binary');
+    response.end();
   }
 };
